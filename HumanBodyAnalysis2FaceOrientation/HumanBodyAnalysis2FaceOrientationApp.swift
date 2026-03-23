cd95884505b77df67c5df65870dac35c5383772b
samtari1 <samtariyang@gmail.com>
2026-03-22 22:41:26 -0400

    Initial Commit

---
+++ HumanBodyAnalysis2FaceOrientation/HumanBodyAnalysis2FaceOrientationApp.swift
@@ -0,0 +1,17 @@
+//
+//  HumanBodyAnalysis2FaceOrientationApp.swift
+//  HumanBodyAnalysis2FaceOrientation
+//
+//  Created by Quanpeng Yang on 3/22/26.
+//
+
+import SwiftUI
+
+@main
+struct HumanBodyAnalysis2FaceOrientationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HumanBodyAnalysis2FaceOrientationTests/HumanBodyAnalysis2FaceOrientationTests.swift
@@ -0,0 +1,17 @@
+//
+//  HumanBodyAnalysis2FaceOrientationTests.swift
+//  HumanBodyAnalysis2FaceOrientationTests
+//
+//  Created by Quanpeng Yang on 3/22/26.
+//
+
+import Testing
+@testable import HumanBodyAnalysis2FaceOrientation
+
+struct HumanBodyAnalysis2FaceOrientationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
